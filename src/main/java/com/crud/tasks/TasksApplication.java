bf559c831d0341c5ab5c1faebf5bc12c8e957064
Mateusz Bialy <Matys@Air-Mateusz.home>
2019-05-16 19:37:08 +0200

    Heroku

--- src/main/java/com/crud/tasks/TasksApplication.java
+++ src/main/java/com/crud/tasks/TasksApplication.java
@@ -2,11 +2,9 @@ package com.crud.tasks;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.builder.SpringApplicationBuilder;
-import org.springframework.boot.web.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class TasksApplication extends SpringBootServletInitializer {
+public class TasksApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(TasksApplication.class, args);
@@ -18,4 +16,3 @@ public class TasksApplication extends SpringBootServletInitializer {
 		//return application.sources(TasksApplication.class);
 	}
 
-}
